659c6d16baafce17317adf6854899b4989a5294f
Lido Stephen <letzbrand@gmail.com>
2025-11-25 15:34:20 +0530

    Configure Render for free tier and production security

--- expense_tracker_core/settings.py
+++ expense_tracker_core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5im8_#o%#&v=k&m5)=jgnh16vhp9%z4tk&eh+)6k^r!5pt7l=="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
